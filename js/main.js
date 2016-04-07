54ef2cc1ab0f667b0b14c47a30a55761dbb433f7
Robin Rakowski <rrakowski1@x9da06.harford.edu>
2016-04-06 21:53:58 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  alert("STILL WORKING");
+
+  $("#meow").click( function() {
+    $("#animals").append("<img class='animal' src='images/flyingkitten.jpg' alt='Flying Kitten' />");
+  });
+
+  $("#snarf").click( function() {
+    $("#animals").append("<img class='animal' src='images/snarf.jpg' alt='Snarf' />");
+  });
+
+  $(".animal").click(function() {
+    this.css('visibility')
+  })
+
 });
